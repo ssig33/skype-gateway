9e7a3bfc9f5a0392d1c73f025bc90367873cf3be
takano32 <takano32@gmail.com>
2009-02-08 21:31:45 +0900

    add test code at hc_skype account

--- SkypeGateway/twitter_client.rb
+++ SkypeGateway/twitter_client.rb
@@ -1,26 +1,28 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
-# IRCで作ろうかと思ったけど、セッションの管理などが難しいので
-# protectedのTwitterで作った方がよさそうだ、などと考えた。
+# IRC で作ろうかと思ったけど、セッションの管理などが難しいので
+# protected の Twitter で作った方がよさそうだ、などと考えた。
 
-
-
+require 'rubygems'
 require 'pit'
+require 'twitter'
 
 class SimpleTwitterClient
 	def initialize
 	end
 end
 
-<<<<<<< HEAD:SkypeGateway/twitter_client.rb
 if __FILE__ == $0 then
 	t = Twitter::Base.new('hc_skype', 'skype_hc')
-	h = t.user(:hirameki)
-	puts "(angel) http://twitter.com/hirameki/status/#{h.status.id}"
-	puts h.status.text.gsub(/&#(\d*?);/) { [$1.to_i].pack('U') }.gsub('&amp;gt;', '>')
+	t.timeline.each do |status|
+		user = status.user.screen_name
+		id = status.id
+		text = status.text.gsub(/&#(\d*?);/) { [$1.to_i].pack('U') }
+		out = "(talk) @#{user} - http://twitter.com/#{user}/status/#{id} \n #{text}"
+		puts '----'
+		puts out
+	end
+	#h = t.user(:hirameki)
+	#puts "(talk) http://twitter.com/hirameki/status/#{h.status.id}"
+	#puts h.status.text.gsub(/&#(\d*?);/) { [$1.to_i].pack('U') }.gsub('&amp;gt;', '>')
 end
-
-
-
-=======
->>>>>>> 311f5b4... add hirameki client:SkypeGateway/twitter_client.rb
