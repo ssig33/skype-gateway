94cc25e699f365b1e95b7da18143a955fde97308
takano32 <takano32@gmail.com>
2009-02-10 07:39:19 +0900

    implement t2s.rb: TwitterToSkype Gateway

---
+++ SkypeGateway/t2s.rb
@@ -0,0 +1,66 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+# 
+# Copyright (c) 2009 TAKANO Mitsuhiro <tak at no32.tk>
+#
+# t2s.rb: TwitterToSkype
+# TwitterのメッセージをSkypeチャットに流します
+#
+
+require 'pit'
+require 'twitter_client'
+require 'skype_client'
+
+class TwitterToSkype
+	def initialize(config)
+		@skype_chat = config['skype_chat']
+		@skype_name = config['skype_name']
+		@twitter_id = config['twitter_id']
+		@twitter_password = config['twitter_password']
+		@skype_client = SimpleSkypeClient.new(@skype_chat)
+		@twitter_client = SimpleTwitterClient.new(@twitter_id, @twitter_password)
+		twitter_initialize
+	end
+	
+	def twitter_initialize
+		@twitter_client.receive_message do |user, url, message|
+			out = "(talk) @#{user} - #{url} \n #{message}"
+			@skype_client.send_message(out)
+		end
+	end
+	
+	def start
+		@skype_client.start
+		@irc_client.start
+	end
+
+	def stop
+		@skype_client.stop
+		@irc_client.stop
+	end	
+end
+
+
+if __FILE__ == $0 then
+	config = Pit.get("TwitterToSkype" + (ARGV[0] ? "-#{ARGV[0]}" : ""),
+						  :require => {
+							  'skype_chat' => 'your skype channel id',
+							  'skype_name' => 'your skype name',
+							  'twitter_id'   => 'bot twitter id',
+							  'twitter_password'   => 'bot twitter password',
+						  })
+
+	gw = TwitterToSkype.new(config)
+	gw.start
+
+	Signal.trap('INT') do
+		gw.stop
+		exit
+	end
+	
+	loop do
+		Thread.pass
+		sleep 0.5
+	end
+end
+
